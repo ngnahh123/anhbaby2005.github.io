a3d2d4216f4913a29da2eac3086d65b88ea7286f
anhbaby2005 <161002986+anhbaby2005@users.noreply.github.com>
2024-03-29 14:34:13 +0700

    bai tap lon

---
+++ PORTFOLIO-BAI TAP LON/main.js
@@ -0,0 +1,7 @@
+// Menu
+const menu = document.querySelector(".navbar__links")
+const menuButton = document.querySelector(".navbar__icons")
+
+menuButton.addEventListener('click', ()=>{
+menu.classList.toggle("navbar__open")
+})
