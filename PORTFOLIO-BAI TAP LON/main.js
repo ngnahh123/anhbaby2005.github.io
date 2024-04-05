c245e4097ba19a73db892b91365992a238b25532
anhbaby2005 <161002986+anhbaby2005@users.noreply.github.com>
2024-04-05 14:45:05 +0700

    about

--- PORTFOLIO-BAI TAP LON/main.js
+++
@@ -1,7 +0,0 @@
-// Menu
-const menu = document.querySelector(".navbar__links")
-const menuButton = document.querySelector(".navbar__icons")
-
-menuButton.addEventListener('click', ()=>{
-menu.classList.toggle("navbar__open")
-})
